07c2e201aa72e025e093ad2c9fd96d74dd16a4fb
Fredrick Brennan <copypaste@kittens.ph>
2023-05-11 02:37:57 -0400

    Add new results from 0.2.0

--- README.md
+++ README.md
@@ -2,14 +2,29 @@
 
 <!-- Rebuild with README.md.sh -->
 
+&copy;2022–2023 Fredrick R. Brennan and `hns` authors. Apache-2 licensed like parent package.
+
 See parent [`hns`](https://github.com/ctrlcctrlv/humnumsort) package.
 
 1. `1000000.txt.zst`
     = one million numbers.
     * regenerate with `seq 0 1000000`
+	- ```bash
+	  		
+		real	0m0.177s
+		user	0m0.083s
+		sys	0m0.094s
+	  ```
 2. `10000000.txt.zst`
     = ten million sequential numbers.
     * regenerate with `seq 0 10000000`
+
+	- ```bash
+	  		
+		real	0m1.894s
+		user	0m0.899s
+		sys	0m0.988s
+	  ```
 3. `0.0.0.0／8.txt.zst`
     = all IPv4 addresses in Class A network №0
     * regenerate with:
@@ -25,14 +40,14 @@ See parent [`hns`](https://github.com/ctrlcctrlv/humnumsort) package.
         done
         ```
 
-    * warning: on my system it takes about as long to generate as it does to sort!
-        ```css
-        {
-            real:    29.471s;
-            user:    27.638s;
-            sys:     1.832s;
-        }
-        ```
+    * <s>warning: on my system it takes about as long to generate as it does to sort!</s>
+       as of 0.2.0 this is fixed, MUCH faster! ≈30x!:
+        - ```bash
+          		
+		real	0m3.211s
+		user	0m1.506s
+		sys	0m1.696s
+          ```
 4. `negatives.txt`
     = 50,000 pseudo-random numbers
     * regenerate with:
@@ -52,3 +67,9 @@ See parent [`hns`](https://github.com/ctrlcctrlv/humnumsort) package.
         
         print(data)
         ```
+        - ```bash
+          		
+		real	0m0.048s
+		user	0m0.041s
+		sys	0m0.006s
+          ```
--- README.md.sh
+++ README.md.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 # Make HTML with `pandoc --highlight-style breezedark -f gfm -t html --self-contained -s README.md -o -`
 # (Optionally) `/usr/bin/xidel --input-format=html --output-format=xml --output-node-indent README.html --xquery "$(cat scripts/xidel.xq)" | xmllint --pretty 1 -`
+test_file() {
+	([ ! -f "$1" ] && unzstd "$1".zst ; time hns < "$1" > /dev/null 2>&1) 2>&1 | sed 's/^/\t\t/'
+}
 cat << EOF
 # Expensive tests for \`hns\` (\`h\`uman \`n\`umeric \`s\`ort)
 
 <!-- Rebuild with README.md.sh -->
 
-ⓒ 2022 Fredrick R. Brennan and \`hns\` authors. Apache-2 licensed like parent package.
+&copy;2022–2023 Fredrick R. Brennan and \`hns\` authors. Apache-2 licensed like parent package.
 
 See parent [\`hns\`](https://github.com/ctrlcctrlv/humnumsort) package.
 
 1. \`1000000.txt.zst\`
     = one million numbers.
     * regenerate with \`seq 0 1000000\`
+	- \`\`\`bash
+	  $( test_file 1000000.txt )
+	  \`\`\`
 2. \`10000000.txt.zst\`
     = ten million sequential numbers.
     * regenerate with \`seq 0 10000000\`
+
+	- \`\`\`bash
+	  $( test_file 10000000.txt )
+	  \`\`\`
 3. \`0.0.0.0／8.txt.zst\`
     = all IPv4 addresses in Class A network №0
     * regenerate with:
@@ -23,18 +33,18 @@ See parent [\`hns\`](https://github.com/ctrlcctrlv/humnumsort) package.
 $(awk '{printf "        %s\n", $0}' < scripts/0.0.0.0／8.sh)
         \`\`\`
 
-    * warning: on my system it takes about as long to generate as it does to sort!
-        \`\`\`css
-        {
-            real:    29.471s;
-            user:    27.638s;
-            sys:     1.832s;
-        }
-        \`\`\`
+    * <s>warning: on my system it takes about as long to generate as it does to sort!</s>
+       as of 0.2.0 this is fixed, MUCH faster! ≈30x!:
+        - \`\`\`bash
+          $( test_file 0.0.0.0／8.txt )
+          \`\`\`
 4. \`negatives.txt\`
     = 50,000 pseudo-random numbers
     * regenerate with:
         \`\`\`python
 $(awk '{printf "        %s\n", $0}' <  scripts/negatives.py)
         \`\`\`
+        - \`\`\`bash
+          $( test_file negatives.txt )
+          \`\`\`
 EOF
